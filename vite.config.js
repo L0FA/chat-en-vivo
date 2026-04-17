b628d4a289b43659bae03034cd7335c2118abc0a
Santiago Lopez Fabbri <santiagolopezfabbri@gmail.com>
2026-04-17 07:55:40 -0300

    Config: change dev port to 8080

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
         host: "0.0.0.0",
-        port: 5173,
+        port: 8080,
         strictPort: false,
         watch: {
             usePolling: true
